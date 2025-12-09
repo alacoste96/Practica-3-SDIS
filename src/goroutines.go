4794c169b835a2c4ddce954a8f4b28dad3f32776
alacoste96 <a.lacoste.r1996@gmail.com>
2025-12-09 20:09:57 +0100

    final-sin-test

---
+++ src/goroutines.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// ====================== Funciones que generan o son hilos ===========================
+
+// cada uno de estos worker gestiona 1 coche en su fase correspondiente
+func worker(g *Garage, entrys [3]chan *Car, exits [3]chan *Car, events chan<- Event, phase int) {
+	var c *Car
+	for {
+		c = getCar(entrys)
+		g.updatePhase(c.id, phase)
+
+		genEvent(events, c, "entra")
+		time.Sleep(c.duration)
+		genEvent(events, c, "sale")
+		if phase == DELIVERYPHASE {
+			g.delCar(c.id)
+			// Liberar una plaza (podrá entrar otro Car en fase 1)
+			g.freeSlots <- struct{}{}
+
+			// Este Car ha terminado TODO el ciclo
+			g.wg.Done()
+		} else {
+			sendCar(exits, c)
+		}
+
+	}
+}
+
+// funcion que genera workers para cada fase
+func startPhase(g *Garage, nWorkers int, entrys [3]chan *Car, exits [3]chan *Car, events chan<- Event, phase int) {
+	for i := 0; i < nWorkers; i++ {
+		go worker(g, entrys, exits, events, phase)
+	}
+}
+
+// ===================== Logger =====================
+
+// este hilo es el único que tiene permitido escribir en stdout
+func logger(events <-chan Event) {
+	for e := range events {
+		secs := e.elapsed.Seconds()
+		fmt.Printf("%-9.2f %-9d %-10s %-6d %-8s\n",
+			secs,
+			e.car,
+			e.issue,
+			e.phase,
+			e.status,
+		)
+
+	}
+}
---
+++ src/utility.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func newGarage(numSlots int) *Garage {
+	g := &Garage{
+		cars:      make(map[int]*Car),
+		freeSlots: make(chan struct{}, numSlots),
+	}
+	for i := 0; i < numSlots; i++ {
+		g.freeSlots <- struct{}{}
+	}
+	return g
+}
+
+func getCarFromQ(cars map[int]*Car) *Car {
+	var chosen *Car
+
+	for id, c := range cars {
+		if c.issue == MECH {
+			chosen = c
+			delete(cars, id)
+			return chosen
+		}
+	}
+	for id, c := range cars {
+		if c.issue == ELECTRIC {
+			chosen = c
+			delete(cars, id)
+			return chosen
+		}
+	}
+	for id, c := range cars {
+		if c.issue == BODY {
+			chosen = c
+			delete(cars, id)
+			return chosen
+		}
+	}
+	return nil
+}
+
+func genCars(ncars int) map[int]*Car {
+	var i int
+
+	cars := make(map[int]*Car, ncars)
+	for i = 0; i < ncars; i++ {
+		cars[i] = genCar(i)
+	}
+	return cars
+}
+
+// ===================== Utilidades =====================
+
+func randDecimal() float64 {
+	n := rand.Intn(21) // 0..20
+	return float64(n) / 10.0
+}
+
+// genera un coche con prio aleatoria
+func genCar(id int) *Car {
+	var issue IssueType
+	var duration time.Duration
+	var lag float64
+	var prio int = rand.Intn(3)
+
+	lag = randDecimal()
+	switch prio {
+	case 0: // mecánica
+		issue = MECH
+		duration = time.Duration((5 + lag) * float64(time.Second))
+	case 1: // eléctrica
+		issue = ELECTRIC
+		duration = time.Duration((3 + lag) * float64(time.Second))
+	case 2: // carroceria
+		issue = BODY
+		duration = time.Duration((1 + lag) * float64(time.Second))
+	}
+
+	return &Car{
+		id:       id,
+		issue:    issue,
+		duration: duration,
+		curphase: 0,
+	}
+}
+
+// obtiene un coche a traves de un canal de comunicacion entre hilos, con prioridades
+func getCar(chans [3]chan *Car) *Car {
+	var c *Car
+
+	select {
+	case c = <-chans[0]: // pillo el de alta prio
+	default:
+		select {
+		case c = <-chans[1]: // pillo el de prio media
+		default:
+			select { // pillo el de baja prio o espero por el primero que llegue
+			case c = <-chans[0]:
+			case c = <-chans[1]:
+			case c = <-chans[2]:
+			}
+		}
+	}
+	return c
+}
+
+// envía un coche a traves de un canal en funcion de su prioridad
+func sendCar(chans [3]chan *Car, c *Car) {
+	switch c.issue {
+	case MECH:
+		chans[0] <- c // prio alta
+	case ELECTRIC:
+		chans[1] <- c // prio media
+	case BODY:
+		chans[2] <- c // prio baja
+	}
+}
+
+// inicializa los canales de una fase (1 por cada prioridad)
+// indice 0 = prio alta. indice 1 = prio media. indice 2 = prio baja
+func initPhaseChans() [3]chan *Car {
+	var chans [3]chan *Car
+
+	for i := range chans {
+		chans[i] = make(chan *Car)
+	}
+	return chans
+}
+
+// genera un evento que se envía al logger para que este escriba por stdout
+func genEvent(events chan<- Event, c *Car, sts string) {
+	events <- Event{
+		elapsed: time.Since(c.start),
+		car:     c.id,
+		phase:   c.curphase,
+		status:  sts,
+		issue:   c.issue,
+	}
+}
